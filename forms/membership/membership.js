0ded51dd170232024729664f483c3e9769587c45
Laura <79476055+LGreenheck@users.noreply.github.com>
2021-03-16 14:15:08 -0500

    Initial commit

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,9 @@
-/*
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+inptMem.oninput=function(members){
+    if checkMember = members[]
+        popup("You are a member")
+    else
+        members.append(checkMember)
+        popup("You are not a member")
+}
