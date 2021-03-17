d475a261a0ed39719ae95f2c224cafe072545e32
Laura <79476055+LGreenheck@users.noreply.github.com>
2021-03-16 20:12:31 -0500

    Update membership.js

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,9 +1,14 @@
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
-inptMem.oninput=function(members){
-    if checkMember = members[]
-        popup("You are a member")
-    else
-        members.append(checkMember)
-        popup("You are not a member")
+
+btnEnter.onclick=function(){
+  if
+  (members.includes(inptMem.value)){
+    lblName.textContent = "You are a member."
+    } else {
+    members.push(inptMem.value)
+    lblName.textContent = "You are not a member."
+    }
 }
+    
+
